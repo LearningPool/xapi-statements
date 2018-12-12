0f8b50b5dda8a79b26fc1ffa6b39381608d6a489
Ryan Smith <0ryansmith1994@gmail.com>
2018-12-12 09:22:14 +0000

    fix: Ignores data outside first and last boundaries for RFC2046.

--- src/expressPresenter/utils/getParts/getStreamParts.ts
+++ src/expressPresenter/utils/getParts/getStreamParts.ts
@@ -1,17 +1,6 @@
-import DataBeforeFirstBoundary from '../../../errors/DataBeforeFirstBoundary';
-import DataBeyondFinalBoundary from '../../../errors/DataBeyondFinalBoundary';
-
 export default (data: string, boundary: string): string[] => {
   const partBoundary = new RegExp(`\\r?\\n?--${boundary}`);
   const dataParts = data.split(partBoundary);
-
-  if (dataParts.length > 0 && dataParts[0] !== '') {
-    throw new DataBeforeFirstBoundary();
-  }
-
-  if (dataParts.length > 1 && dataParts[dataParts.length - 1] !== '--') {
-    throw new DataBeyondFinalBoundary();
-  }
-
+  // Slices to ignore data before the first boundary and after the last boundary.
   return dataParts.slice(1, -1);
 };
